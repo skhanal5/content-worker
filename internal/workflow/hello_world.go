fa33c0f39870facdf5a5d81c1836bdb80bf9deab
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-05-03 13:33:36 -0400

    Define worker, workflow, and activity for hello world sample

---
+++ internal/workflow/hello_world.go
@@ -0,0 +1,29 @@
+package workflow
+
+import (
+	"clip-farmer-workflow/internal/activity"
+	"time"
+
+	"go.temporal.io/sdk/workflow"
+)
+
+type HelloWorldInput struct {
+	Name string `json:"name,omitempty"`
+}
+
+type HelloWorldOutput struct {
+	Message string `json:"message,omitempty"`
+}
+
+func HelloWorldWorkflow(ctx workflow.Context, input HelloWorldInput) (*HelloWorldOutput, error) {
+	activityOptions := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, activityOptions)
+	var output *HelloWorldOutput
+	err := workflow.ExecuteActivity(ctx, activity.HelloWorldActivity, input.Name).Get(ctx, &output.Message)
+	if err != nil {
+		return nil, err
+	}
+	return output, err
+}
