fa603313e8a141c7f1d60a8385b7ad8b975c6346
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-05-03 13:34:53 -0400

    Update main function to define temporal client at localhost and to start the worker

--- cmd/clip-farmer-workflow/main.go
+++ cmd/clip-farmer-workflow/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"clip-farmer-workflow/internal"
+	"log"
+
+	"go.temporal.io/sdk/client"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	temporalClient, err := client.Dial(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		log.Fatalln("Unable to create Temporal client", err)
+	}
+	defer temporalClient.Close()
+	internal.StartWorker(temporalClient)
 }
