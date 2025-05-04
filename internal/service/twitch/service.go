0b60455346eaf5d80a65ff949253530051bc94f8
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-05-03 20:01:19 -0400

    Define service directory which will contain IO op's with downstream services. Add twitch service impl with functionality to get clips and broadcaster information

---
+++ internal/service/twitch/service.go
@@ -0,0 +1,75 @@
+package twitch
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const (
+	oauthBaseURL = "https://id.twitch.tv/oauth2/token?client_id=%s&grant_type=client_credentials"
+)
+
+type TwitchManager interface {
+	GetBroadcaster(broadcasterId string) (string, error)
+	GetClips(username string) ([]string, error)
+}
+
+type TwitchService struct {
+	client  *resty.Client
+	authURL string
+}
+
+func (t *TwitchService) GetBroadcaster(username string) (*UsersResponse, error) {
+	resp, err := t.client.R().
+		SetQueryParam("first", "1").
+		SetResult(&UsersResponse{}).
+		Get("/users?login=" + username)
+	if err != nil {
+		return &UsersResponse{}, err
+	}
+	return resp.Result().(*UsersResponse), nil
+}
+
+func (t *TwitchService) GetClips(broadcasterId string) (*UsersResponse, error) {
+	resp, err := t.client.R().
+		SetQueryParam("first", "1").
+		SetResult(&UsersResponse{}).
+		Get("/clips?broadcaster_id=" + broadcasterId)
+	if err != nil {
+		return &UsersResponse{}, err
+	}
+	return resp.Result().(*UsersResponse), nil
+}
+
+func RefreshAuthMiddleware(c *resty.Client, res *resty.Response) error {
+	if res.StatusCode() == http.StatusUnauthorized {
+		authURL := fmt.Sprintf(oauthBaseURL, c.Header.Get("Client-ID"))
+		resp, err := c.R().Post(authURL)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode() != http.StatusOK {
+			return fmt.Errorf("failed to refresh auth token: %s", resp.String())
+		}
+		var authResponse AuthResponse
+		if err := json.Unmarshal(resp.Body(), &authResponse); err != nil {
+			return err
+		}
+		c.SetHeader("Authorization", "Bearer "+authResponse.AccessToken)
+	}
+	return nil
+}
+
+func NewTwitchClient(baseURL string, clientId string) *TwitchService {
+	return &TwitchService{
+		client: resty.New().
+			SetTimeout(10*time.Second).
+			SetBaseURL(baseURL).
+			SetHeader("Client-ID", clientId).
+			OnAfterResponse(RefreshAuthMiddleware),
+	}
+}
