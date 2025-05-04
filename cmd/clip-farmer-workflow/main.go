43b6a3d8a5b18b667ad0ed9b8e4c9e3a7ce719c0
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-05-03 21:22:42 -0400

    Update twitch client secret env var naming in entrypoint. Update readme to include env vars

--- cmd/clip-farmer-workflow/main.go
+++ cmd/clip-farmer-workflow/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	cfg := config.Config{
 		TwitchClientId:     config.GetEnv("TWITCH_CLIENT_ID", ""),
-		TwitchClientSecret: config.GetEnv("TWITCH_CLIENT", ""),
+		TwitchClientSecret: config.GetEnv("TWITCH_CLIENT_SECRET", ""),
 		TwitchBaseURL:      config.GetEnv("TWITCH_BASE_URL", "https://api.twitch.tv/helix"),
 	}
 	worker.StartWorker(cfg)
