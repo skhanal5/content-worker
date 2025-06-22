25a2c0c0434a97710f53394e5abcee89319279da
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-06-21 21:10:57 -0400

    WIP: Update activity struct to not include hello world and edit since those are pure functions. Add business logic to takea passed in style and convert it to edit opts and call render method.

--- internal/activity/activity.go
+++ internal/activity/activity.go
@@ -3,7 +3,6 @@ package activity
 import (
 	"clip-farmer-workflow/internal/config"
 	"clip-farmer-workflow/internal/service/download"
-	"clip-farmer-workflow/internal/service/edit"
 	"clip-farmer-workflow/internal/service/helloworld"
 	"clip-farmer-workflow/internal/service/twitch"
 )
@@ -12,19 +11,13 @@ type Activity struct {
 	helloworld.HelloWorldManager
 	twitch.TwitchManager
 	download.DownloadManager
-	edit.EditManager
 }
 
 func NewActivity(cfg config.Config) *Activity {
-	helloManager := helloworld.HelloWorldService{}
 	twitchManager := twitch.NewTwitchService(cfg.TwitchHelixClientId, cfg.TwitchHelixSecret, cfg.TwitchGQLClientId)
-
 	downloadManager := download.NewDownloadService()
-	editManager := edit.EditService{}
 	return &Activity{
-		HelloWorldManager: helloManager,
 		TwitchManager:     twitchManager,
 		DownloadManager:  downloadManager,
-		EditManager: editManager,
 	}
 }
